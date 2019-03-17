1406a16e9dbc5c0d65492b7ddf26da33a86c10a1
dexterastin <dexterastin@gmail.com>
2019-03-17 18:29:20 +0900

    solve homework 1

--- homework/00.Math/skeleton_codes/mat.hpp
+++ homework/00.Math/skeleton_codes/mat.hpp
@@ -6,91 +6,86 @@
 #include <cstdarg>
 
 namespace kmuvcl {
-  namespace math {
+    namespace math {
 
-    template <unsigned int M, unsigned int N, typename T>
-    class mat
-    {
-    public:
-      mat()
-      {
-        set_to_zero();
-      }
+        template<unsigned int M, unsigned int N, typename T>
+        class mat {
+        public:
+            mat() {
+                set_to_zero();
+            }
 
-      mat(const T elem)
-      {
-        std::fill(val, val + M*N, elem);
-      }
+            mat(const T elem) {
+                std::fill(this->val, this->val + M * N, elem);
+            }
 
-      T& operator()(unsigned int r, unsigned int c)
-      {
-        // TODO: Fill up this function properly 
-        // Notice: The matrix is column major
-        return  val[0];
-      }
+            T &operator()(unsigned int r, unsigned int c) {
+                return val[r + c * M];
+            }
 
-      const T& operator()(unsigned int r, unsigned int c) const
-      {
-        // TODO: Fill up this function properly 
-        // Notice: The matrix is column major
-        return  val[0];   
-      }
+            const T &operator()(unsigned int r, unsigned int c) const {
+                return val[r + c * M];
+            }
 
-      // type casting operators
-      operator const T* () const
-      {
-        return  val;
-      }
+            // type casting operators
+            operator const T *() const {
+                return val;
+            }
 
-      operator T* ()
-      {
-        return  val;
-      }
+            operator T *() {
+                return val;
+            }
 
-      void set_to_zero()
-      {
-        // TODO: Fill up this function properly 
-      }
+            void set_to_zero() {
+                std::fill(this->val, this->val + M * N, 0);
+            }
 
-      void get_ith_column(unsigned int i, vec<M, T>& col) const
-      {
-        // TODO: Fill up this function properly 
-      }
+            void get_ith_column(unsigned int i, vec<M, T> &col) const {
+                for(unsigned int j = 0; j < M; j++) {
+                    col(j) = (*this)(j, i);
+                }
+            }
 
-      void set_ith_column(unsigned int i, const vec<M, T>& col)
-      {
-        // TODO: Fill up this function properly 
-      }
+            void set_ith_column(unsigned int i, const vec<M, T> &col) {
+                for(unsigned int j = 0; j < M; j++) {
+                    (*this)(j, i) = col(j);
+                }
+            }
 
-      void get_ith_row(unsigned int i, vec<N, T>& row) const
-      {
-       // TODO: Fill up this function properly 
-      }
+            void get_ith_row(unsigned int i, vec<N, T> &row) const {
+                for(unsigned int j = 0; j < N; j++) {
+                    row(j) = (*this)(i, j);
+                }
+            }
 
-      void set_ith_row(unsigned int i, const vec<N, T>& row)
-      {
-        // TODO: Fill up this function properly 
-      }
+            void set_ith_row(unsigned int i, const vec<N, T> &row) {
+                for(unsigned int j = 0; j < N; j++) {
+                 (*this)(i, j) = row(j);
+                }
+            }
 
-      mat<N, M, T> transpose() const
-      {
-        // TODO: Fill up this function properly 
-        mat<N, M, T>  trans;
-        
-        return  trans;
-      }
+            mat<N, M, T> transpose() const {
+                mat<N, M, T> trans;
+                vec<M, T> col;
 
-    protected:
-      T val[M*N];   // column major
-    };
+                for(unsigned int i = 0; i < N; i++) {
+                    this->get_ith_column(i, col);
+                    trans.set_ith_row(i, col);
+                }
+                return trans;
+            }
 
-    typedef mat<3, 3, float>    mat3x3f;
-    typedef mat<3, 3, double>   mat3x3d;
+        protected:
+            T val[M * N];   // column major
+        };
 
-    typedef mat<4, 4, float>    mat4x4f;
-    typedef mat<4, 4, double>   mat4x4d;
+        typedef mat<3, 3, float> mat3x3f;
+        typedef mat<3, 3, double> mat3x3d;
 
-  } // math
+        typedef mat<4, 4, float> mat4x4f;
+        typedef mat<4, 4, double> mat4x4d;
+
+    } // math
 } // kmuvcl
 
 #include "operator.hpp"
--- homework/00.Math/skeleton_codes/vec.hpp
+++ homework/00.Math/skeleton_codes/vec.hpp
@@ -5,104 +5,94 @@
 #include <algorithm>
 
 namespace kmuvcl {
-  namespace math {
+    namespace math {
 
-    template <unsigned int N, typename T>
-    class vec
-    {
-    public:
-      vec()
-      {
-        set_to_zero();
-      }
+        template<unsigned int N, typename T>
+        class vec {
+        public:
+            vec() {
+                set_to_zero();
+            }
 
-      vec(const T elem)
-      {
-        // TODO: Fill up this function properly 
-      }
+            vec(const T elem) {
+                std::fill(this->val, this->val + N, elem);
+            }
 
-      vec(const T s, const T t) : vec()
-      {
-        // TODO: Fill up this function properly 
-      }
+            vec(const T s, const T t) : vec() {
+                this->val[0] = s;
+                this->val[1] = t;
+            }
 
-      vec(const T s, const T t, const T u) : vec()
-      {
-        // TODO: Fill up this function properly 
-      }
+            vec(const T s, const T t, const T u) : vec(s, t) {
+                this->val[2] = u;
+            }
 
-      vec(const T s, const T t, const T u, const T v) : vec()
-      {
-        // TODO: Fill up this function properly 
-      }
-      
-      vec(const vec<N, T>& other)
-      {
-        // TODO: Fill up this function properly 
-      }
+            vec(const T s, const T t, const T u, const T v) : vec(s, t, u) {
+                this->val[3] = v;
+            }
 
-      vec& operator= (const vec<N, T>& other)
-      {
-        // TODO: Fill up this function properly 
-        return  *this;
-      }
+            vec(const vec<N, T> &other) {
+                std::copy(other.val, other.val + N, this->val);
+            }
 
-      T& operator()(unsigned int i)
-      {
-	      // TODO: Fill up this function properly 
-        return  val[0];
-      }
+            vec &operator=(const vec<N, T> &other) {
+                for(int i = 0; i < N; i++) {
+                    this->val[i] = other.val[i];
+                }
+                return *this;
+            }
 
-      const T& operator()(unsigned int i) const
-      {
-	      // TODO: Fill up this function properly 
-        return  val[0];
-      }
+            T &operator()(unsigned int i) {
+                return val[i];
+            }
 
-      // type casting operators
-      operator const T* () const
-      {
-        return  val;
-      }
+            const T &operator()(unsigned int i) const {
+                return val[i];
+            }
 
-      operator T* ()
-      {
-        return  val;
-      }
+            // type casting operators
+            operator const T *() const {
+                return val;
+            }
 
-      vec& operator+=(const vec<N, T>& other)
-      {
-        // TODO: Fill up this function properly 
-        return *this;
-      }
+            operator T *() {
+                return val;
+            }
 
-      vec& operator-=(const vec<N, T>& other)
-      {
-        // TODO: Fill up this function properly 
-        return *this;
-      }
+            vec &operator+=(const vec<N, T> &other) {
+                for(unsigned int i = 0; i < N; i++) {
+                    this->val[i] += other.val[i];
+                }
+                return *this;
+            }
 
-      void set_to_zero()
-      {
-        // TODO: Fill up this function properly 
-      }
+            vec &operator-=(const vec<N, T> &other) {
+                for(unsigned int i = 0; i < N; i++) {
+                    this->val[i] -= other.val[i];
+                }
+                return *this;
+            }
 
-    protected:
-      T val[N];
-    };
+            void set_to_zero() {
+                std::fill(this->val, this->val + N, 0);
+            }
 
-    // typedef
-    typedef vec<2, float>   vec2f;
-    typedef vec<2, double>  vec2d;
+        protected:
+            T val[N];
+        };
 
-    typedef vec<3, float>   vec3f;
-    typedef vec<3, double>  vec3d;
-    
-    typedef vec<4, float>   vec4f;
-    typedef vec<4, double>  vec4d;
+        // typedef
+        typedef vec<2, float> vec2f;
+        typedef vec<2, double> vec2d;
+
+        typedef vec<3, float> vec3f;
+        typedef vec<3, double> vec3d;
+
+        typedef vec<4, float> vec4f;
+        typedef vec<4, double> vec4d;
 
 
-  } // namespace math
+    } // namespace math
 } // namespace kmuvcl
 
 
